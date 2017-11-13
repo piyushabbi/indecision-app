49d13caa8ae1b3caf9824c3f1db641f865d595d5
Piyush Abbi <piyushabbi27@gmail.com>
2017-11-13 15:39:19 +0530

    Added Editor config

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,34 @@
-console.log('App js Running !');
+"use strict";
 
-// JSX
-var template = React.createElement(
-  "h1",
-  { id: "heading", className: "heading", "data-heading": "heading" },
-  "This is a jsx"
+console.log("App js Running !");
+
+var template2 = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "Piyush"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Age: 26"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Location: Bengaluru"
+  ),
+  React.createElement(
+    "ul",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "Play Basketball"
+    )
+  )
 );
 
-ReactDOM.render(template, document.getElementById('root'));
+ReactDOM.render(template2, document.getElementById("root"));
---
+++ src/app.js
@@ -0,0 +1,14 @@
+console.log("App js Running !");
+
+var template2 = (
+  <div>
+    <h1>Piyush</h1>
+    <p>Age: 26</p>
+    <p>Location: Bengaluru</p>
+    <ul>
+        <li>Play Basketball</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(template2, document.getElementById("root"));
