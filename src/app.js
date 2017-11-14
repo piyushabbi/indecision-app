a9ff8f02fdc5654fe9f619ebd9f1cf36987d6979
Piyush Abbi <piyushabbi27@gmail.com>
2017-11-14 15:22:14 +0530

    Added Transform Class Properties
    Also added a todo for the tasks to be done.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+/**
+ * TODO
+ * 1. Wrapper
+ * 2. Header
+ * 3. Random CTA Button
+ * 4. Todo List Component
+ *  a. Delete for each todo item
+ * 5. Form input and Submit
+ *  a. Not able to Submit if the todo already exists
+ */
+
 render(<h1>Indecision App</h1>, document.getElementById('root'));
